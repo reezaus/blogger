f57c139cafcbca3933399124d63e03da1173973e
Joseph Reed <jereed559@gmail.com>
2019-07-01 09:08:23 -0700

    functionality improvements

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,43 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
     def index
         @articles = Article.all
     end
 
+    def new
+        @article = Article.new
+    end
+
     def show
     @article = Article.find(params[:id])
+    end
+
+    def create
+        @article = Article.new(article_params)
+        @article.save
+
+        redirect_to article_path(@article)
+        flash.notice = "Article '#{@article.title}' Created!"
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+
+        redirect_to articles_path
+        flash.notice = "Article '#{@article.title}' Deleted!"
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+
+        flash.notice = "Article '#{@article.title}' Updated!"
+
+        redirect_to article_path(@article)
     end
 end
